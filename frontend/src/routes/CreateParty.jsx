b74d1f923796b46a31e8628987d429fe82682063
Lucas <lucaslops12@gmail.com>
2023-12-28 18:41:05 -0300

    criação de festas

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,11 +1,135 @@
-import React from 'react'
+import partyFetch from "../axios/config";
+
+import { useState, useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import "./Form.css";
 
 const CreateParty = () => {
-  return (
-    <div>
-      festa
-    </div>
-  )
-}
+  const [services, setservices] = useState([]);
 
-export default CreateParty
+  const [title, settitle] = useState("");
+  const [author, setauthor] = useState("");
+  const [description, setdescription] = useState("");
+  const [budget, setbudget] = useState(0);
+  const [image, setimage] = useState("");
+  const [partyServices, setpartyServices] = useState([]);
+
+  useEffect(() => {
+    const loadServices = async () => {
+      const res = await partyFetch.get("/services");
+
+      setservices(res.data);
+    };
+    loadServices();
+  }, []);
+
+  const handleServices = (e) =>{
+    const checked = e.target.checked
+    const value = e.target.value
+
+    const filteredService = services.filter((s) => s._id === value)
+
+    if(checked){
+      setpartyServices((services) => [...services, filteredService[0]])
+    }else{
+      setpartyServices((services) => services.filter((s) => s._id !== value ))
+    }
+
+  }
+
+  const createParty = (e) => {
+    e.preventDefault();
+
+    const party = {
+      title,
+      author,
+      description,
+      budget,
+      image,
+      services: partyServices,
+    };
+
+ 
+    
+  };
+  return (
+    <div className="form-page">
+      <h2>Crie sua próxima Festa</h2>
+      <p>Defina o seu orçamento e escolha os serviços</p>
+      <form onSubmit={(e) => createParty(e)}>
+        <label>
+          <span>Nome da festa:</span>
+          <input
+            type="text"
+            placeholder="Seja criativo..."
+            required
+            onChange={(e) => settitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Anfitrião:</span>
+          <input
+            type="text"
+            placeholder="Quem está dando a festa?"
+            required
+            onChange={(e) => setauthor(e.target.value)}
+            value={author}
+          />
+        </label>
+        <label>
+          <span>Descricão:</span>
+          <textarea
+            placeholder="Conte mais sobre a festa"
+            required
+            onChange={(e) => setdescription(e.target.value)}
+            value={description}
+          ></textarea>
+        </label>
+        <label>
+          <span>Orçamento:</span>
+          <input
+            type="nember"
+            placeholder="Quanto vc pretende investir"
+            required
+            onChange={(e) => setbudget(e.target.value)}
+            value={budget}
+          />
+        </label>
+        <label>
+          <span>Imagem</span>
+          <input
+            type="text"
+            placeholder="URL da imagem"
+            required
+            onChange={(e) => setimage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <div>
+          <h2>Escolha os serviços</h2>
+          <div className="services-container">
+            {services.length === 0 && <p>Carregando...</p>}
+            {services.length > 0 &&
+              services.map((service) => (
+                <div className="service" key={service._id}>
+                  <img src={service.image} alt={service.name} />
+                  <p className="service-name">{service.name}</p>
+                  <p className="service-price">R${service.price}</p>
+                  <div className="checkbox-container">
+                    <input type="checkbox" value={service._id} onChange={(e) => handleServices(e)} />
+                    <p>Marque para solicitar</p>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+        <input type="submit" value="Criar Festa" className="btn" />
+      </form>
+    </div>
+  );
+};
+
+export default CreateParty;
